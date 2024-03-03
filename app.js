0d0fe3ae0084265e907fd4c839eb0ebedb964943
Mohsin Khan <muhammadmohsinkhan.aptech@gmail.com>
2024-03-03 21:13:42 +0500

    made responsive

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ let navlinks = document.querySelectorAll("nav .navlink");
 let navlist = document.querySelector("nav > .navlist");
 let navContainer = document.querySelector(".header-container");
 let nav = document.querySelector(".nav");
+let navIconContainer = document.querySelector(".nav-icon-container");
+let navIcon = document.querySelector("#nav-icon");
 let scrollToTopBtn = document.querySelector(".scroll-to-top-container");
 
 // Function to dim the non-active navlinks when hovering on a navlink
 navlinks.forEach((navlink) => {
 	navlink.addEventListener("mouseenter", navlinkDim);
 	navlink.addEventListener("mouseleave", navlinkDim);
+	navlink.addEventListener("click", navToggle); // for hiding nav-panel in mobiles
 });
 function navlinkDim() {
 	navlist.classList.toggle("navlist-dim");
@@ -45,4 +48,11 @@ function navHide() {
 scrollToTopBtn.addEventListener("click", scrollToTop);
 function scrollToTop() {
 	window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+// hamburger menu
+navIconContainer.addEventListener("click", navToggle);
+function navToggle() {
+	navIcon.classList.toggle("open");
+	nav.classList.toggle("nav-toggle");
 }
